2368f38a15b0fea1a8cd080567d8f861ffd4304f
Wafa-Abbassi <32781096+Wafa-Abbassi@users.noreply.github.com>
2018-12-26 20:47:07 +0100

    Create basic.js

---
+++ 0-basic/basic.js
@@ -0,0 +1,11 @@
+for (var i=1; i<=100; i++) {
+    if (i%15===0) {
+        console.log("Babasport");
+    } else if (i%3===0) {
+        console.log("Baba");
+    } else if (i%5===0) {
+        console.log("Sport");
+    } else {
+        console.log(i)
+    }
+}
